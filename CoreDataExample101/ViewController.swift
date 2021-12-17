6192dbfc5e6b4b03eaeb82e8d1ca552cb726890e
Ahmed Qazzaz <aqazzaz2@hotmail.com>
2021-12-17 20:55:39 +0200

    initial commit

---
+++ CoreDataExample101/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CoreDataExample101
+//
+//  Created by Ahmed Qazzaz on 15/12/2021.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        Admin()
+    }
+
+
+}
+
